92e8169cc717f07fd67733095983f82dc09562d5
Rojda <160949927+rojda-o@users.noreply.github.com>
2024-10-09 20:28:57 +0300

    Update firstScript.cs

--- firstScript.cs
+++ firstScript.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class firstScript : MonoBehaviour
+public class Cube1_Script : MonoBehaviour
 {
-    public float speed_1 = 1.0f; // Public değişken, Inspector'da görünür
+    public float speed_1 = 1.0f;  //public degisken Inspector'da görünür
+    private float speed_2 = 0.5f; //private degisken sadece bu script icinde kullanılabilir
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(5, 0, 0); // Küpü x ekseninde tasima
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveVertical = Input.GetAxis("Vertical") * speed_1 * Time.deltaTime; 
-        transform.position += new Vector3(0, moveVertical, 0); // Y ekseninde hareket 
+        float moveVertical = Input.GetAxis("Vertical") * speed_1 * Time.deltaTime;
+        transform.position += new Vector3(0, moveVertical, 0); // Y ekseninde hareket
 
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed_1 * Time.deltaTime;
+        transform.position += new Vector3(moveHorizontal, 0, 0); // X ekseninde hareket
+        */
     }
 }
